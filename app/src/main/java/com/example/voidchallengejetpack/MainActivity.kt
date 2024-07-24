9e5dc15def21bc115b555721d32bcfc938f776de
jonib <jonibpereira@gmail.com>
2024-07-24 23:58:53 +0100

    Raw UI done: Needs refinement and null check/protection

--- app/src/main/java/com/example/voidchallengejetpack/MainActivity.kt
+++ app/src/main/java/com/example/voidchallengejetpack/MainActivity.kt
@@ -8,74 +8,39 @@ import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-//import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-//import com.example.voidchallengejetpack.data.models.TVShowListEntry
-// dagger.hilt.android.AndroidEntryPoint
+import com.example.voidchallengejetpack.showdetails.ShowDetailsScreen
+import com.example.voidchallengejetpack.showslist.ShowListViewModel
+import com.example.voidchallengejetpack.showslist.TVShowListScreen
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.serialization.Serializable
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.Objects
-
 
 // TODO talvez guardar os populares no primeiro request e apenas refazer o pedido se for dado refresh: mais rapido
 
-const val API_KEY = "39a3c712614c598a6d5ca7a7c35a3ab1"
-const val IMG_PATH_400 = "https://image.tmdb.org/t/p/w400"
-const val IMG_PATH_300 = "https://image.tmdb.org/t/p/w300"
-const val IMG_PATH_200 = "https://image.tmdb.org/t/p/w200"
-
-var tvShowList by mutableStateOf<List<TVShow>>(emptyList())
 var tvShowGenresList by mutableStateOf(mutableMapOf<Int, String>())
-var displayedText by mutableStateOf("")
-var fetchingResults by mutableStateOf(false)
+var displayedText by mutableStateOf("Popular right now:")
 
 
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
+    //private val viewModel by viewModels<MainViewModel>()
+    private val showsViewModel by viewModels<ShowListViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
             setKeepOnScreenCondition {
-                !viewModel.isReady.value
+                !showsViewModel.isReady.value
             }
             setOnExitAnimationListener { screen ->
                 val zoomX = ObjectAnimator.ofFloat(
@@ -120,15 +85,16 @@ class MainActivity : ComponentActivity() {
 
             NavHost(
                 navController = navController,
-                startDestination = MainScreen,
+                startDestination = TVShowListMainScreen,
             ) {
-                composable<MainScreen> {
-                    MyApp(navController)
+                composable<TVShowListMainScreen> {
+                    TVShowListScreen(navController = navController, viewModel = showsViewModel)
                 }
 
                 composable<TVShowDetailsScreen> {
                     val args = it.toRoute<TVShowDetailsScreen>()
-                    TVShowDetails(tvShowId = args.id)
+                    ShowDetailsScreen(showId = args.id, navHostController = navController)
+                    //TVShowDetails(tvShowId = args.id, viewModel = showsViewModel)
                 }
             }
         }
@@ -137,7 +103,7 @@ class MainActivity : ComponentActivity() {
 
 // Main Screen
 @Serializable
-object MainScreen
+object TVShowListMainScreen
 
 // Second Screen
 @Serializable
@@ -145,137 +111,7 @@ data class TVShowDetailsScreen(
     val id: Int
 )
 
-@Composable
-fun MyApp(navController: NavHostController,
-    ) {
-
-    var searchText by remember { mutableStateOf("") }
-
-    val coroutineScope = rememberCoroutineScope()
-
-    // loading data is finished or not
-    val finish = remember {
-        mutableStateOf(false)
-    }
-
-    Surface(color = colorResource(id = R.color.black)) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-        ) {
-
-            Image(
-                painter = painterResource(id = R.drawable.splash_logo),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(top = 8.dp)
-
-            )
-
-            Row (
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
-            ){
-
-                SearchBar(
-
-                    onSearch = { newText ->
-                        // Handle search functionality here
-                        searchText = newText
-                        println("Search text: $searchText")
-                        if (searchText.isNotEmpty()) { // Perform search and update movieList
-
-                            performSearchAsync(scope = coroutineScope,
-                                searchText,
-                                onSuccess = { response ->
-                                    // Handle successful response and update UI
-                                    tvShowList = setShowList(response)
-                                    displayedText = if (tvShowList.isNotEmpty()) {
-                                        "Search results for: $searchText"
-                                    } else {
-                                        "No results found for: $searchText"
-                                    }
-                                },
-                                onFailure = { exception ->
-                                    // Handle network request failure
-                                    println("Error: ${exception.message}")
-                                    displayedText = "Check your internet connection..."
-                                }
-                            )
-                        } else { // No search text, get popular shows
-                            getPopularShowsAsync(
-                                onSuccess = { response ->
-                                    // Handle successful response and update UI
-                                    tvShowList = setShowList(response)
-                                    displayedText = if (tvShowList.isNotEmpty()) {
-                                        "Popular right now:"
-                                    } else {
-                                        "Nothing popular right now"
-                                    }
-                                },
-                                onFailure = { exception ->
-                                    // Handle network request failure
-                                    println("Error: ${exception.message}")
-                                    displayedText = "Check your internet connection..."
-                                }
-                            )
-
-                        }
-                    }
-                )
-                }
-
-            Text(
-                text = displayedText,
-                color = Color.White,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-
-
-            )
-
-            if (fetchingResults) {
-                displayedText = ""
-                LoadingScreen()
-
-            }
-
-            else {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(.90f)
-                        .background(Color.Black),
-                ) {
-                    items(
-
-                        items = tvShowList,
-                        key = {
-                            it.id
-                        }
-                    ) { tvShow ->
-                        TVShowItemLighter(tvShow = tvShow, navController, fetchingResults)
-                        HorizontalDivider()
-                    }
-
-                }
-            }
-
-            Text(
-                text = "Developed by Jóni Pereira as a VOID's challenge",
-                color = Color.Gray,
-                fontSize = 12.sp,
-                modifier = Modifier
-                    .padding(16.dp)
-
-            )
-        }
-    }
-}
-
+/*
 fun setShowList(response: String) : List<TVShow> {
     val results = extractResults("results", response)
     if (results != null) {
@@ -325,7 +161,10 @@ fun setGenres(response: String) : MutableMap<Int, String> {
     }
     return mutableMapOf()
 }
+*/
 
+
+/*
 /**
  * Extracts the "tag" array from the JSON response
  */
@@ -339,213 +178,5 @@ fun extractResults(tag: String, responseBody: String?): JSONArray? {
         e.printStackTrace()
     }
     return null
-}
-
-/*
-@Composable
-fun MovieItem(title: String, date: String, genres: String, cast: String, description: String, rating: Int, duration: String, imagePosterURL: String, imageBackgroundURL: String) {
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Row( // single row
-            modifier = Modifier
-                .background(color = colorResource(id = R.color.themeColorPrim))
-                .height(160.dp)
-                .padding(4.dp),
-
-            horizontalArrangement = Arrangement.Center,
-
-
-            ) {
-
-            RemoteImage( // Poster
-                url = imagePosterURL,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .height(160.dp)
-            )
-
-            /*
-            // For preview purpose
-            Image(
-                painter = painterResource(id = R.drawable.movie_poster),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxHeight()
-            )
-             */
-
-            Box() {
-                Column(
-                    // Column for remaining info
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                ) {
-
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = title,
-                            fontSize = 16.sp,
-                            color = colorResource(id = R.color.white)
-                        )
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        RatingBar(
-                            maxStars = 5,
-                            rating = rating
-                        )
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        Text(
-                            text = duration,
-                            fontSize = 10.sp,
-                            color = colorResource(id = R.color.grayish_text)
-                        )
-
-                    }
-
-                    Spacer(modifier = Modifier.height(2.dp))
-
-                    Row(
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-
-                        Text(
-                            text = date,
-                            fontSize = 10.sp,
-                            color = colorResource(id = R.color.grayish_text)
-                        )
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        Text(
-                            text = genres,
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            fontSize = 10.sp,
-                            color = colorResource(id = R.color.grayish_text)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    Text(
-                        text = "Cast",
-                        fontSize = 10.sp,
-                        color = colorResource(id = R.color.white)
-                    )
-                    Spacer(modifier = Modifier.height(2.dp))
-
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-
-                        maxLines = 2,
-                        text = cast,
-                        fontSize = 10.sp,
-                        color = colorResource(id = R.color.grayish_text)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    Text(
-                        text = "Description",
-                        fontSize = 10.sp,
-                        color = colorResource(id = R.color.white)
-                    )
-
-                    Spacer(modifier = Modifier.height(2.dp))
-
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-
-                        maxLines = 2,
-                        text = description,
-                        fontSize = 10.sp,
-                        color = colorResource(id = R.color.grayish_text)
-                    )
-
-                    Image(
-                        painter = painterResource(id = R.drawable.movie_poster),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .height(160.dp)
-                            .padding(end = 8.dp)
-                            .alpha(0.2f)
-
-
-                    )
-                }
-
-                RemoteImage(
-                    url = imageBackgroundURL,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(160.dp)
-                        .align(Alignment.BottomStart)
-                        .alpha(0.2f)
-                )
-                /*
-                // For preview purpose
-                Image( // Poster
-                painter = painterResource(id = R.drawable.movie_poster),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxHeight()
-            )
-                 */
-            }
-        }
-
-        /*Image(
-            painter = painterResource(id = R.drawable.movie_poster),
-            contentDescription = null,
-            modifier = Modifier
-                .height(160.dp)
-                .align(Alignment.CenterEnd)
-                .padding(end = 8.dp)
-                .alpha(0.2f)
-
-
-        )*/
-    }
-}
-
-@Composable
-fun RatingBar(
-    maxStars: Int = 5,
-    rating: Int,
-) {
-    Row {
-        for (i in 1..maxStars) {
-            Icon(
-                imageVector = Icons.Filled.Star,
-                contentDescription = null,
-                tint = if (i <= rating) colorResource(id = R.color.themeColorSec) else colorResource(id = R.color.black).copy(alpha = 0.8f),
-                modifier = Modifier
-                    .size(8.dp)
-            )
-        }
-    }
-}
-
-@Composable
-fun RemoteImage(url: String, modifier: Modifier = Modifier) {
-    val painter: Painter = rememberImagePainter(url)
-    Image(
-        painter = painter,
-        contentDescription = null,
-        modifier = modifier
-    )
 }
 */
